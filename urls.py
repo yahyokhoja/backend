ce102151124c675c8eb941ae526e5decd5a79693
yahyokhoja <yahuokhoja@gmail.com>
2025-04-08 07:57:30 +0500

    secret

--- urls.py
+++ urls.py
@@ -4,8 +4,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("✅ Django работает! Это главная страница.")
-
+    return HttpResponse("✅ Django работает! Это главная")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', lambda request: HttpResponse("🩺 Всё хорошо!")),  # для /health
